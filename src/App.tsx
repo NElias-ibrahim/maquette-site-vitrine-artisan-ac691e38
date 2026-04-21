314b0b61e41b50f8ec06ec6f5d35134b07a27935
Elias Nassiri <eliasnassiri09@gmail.com>
2026-04-21 18:42:45 +0200

    git hub page

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           {/* Routes principales avec layout commun */}
           <Route element={<Layout />}>
